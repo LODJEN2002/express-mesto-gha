ed2ce41ba906861bcb6e2aa07901aa80b8964d11
Ivan Docenko <decel1337@gmail.com>
2022-11-18 15:35:14 +0300

    express-mesto-gha

--- routes/cards.js
+++ routes/cards.js
@@ -23,19 +23,19 @@ router.get('/', getCards);
 
 router.delete('/:cardId', celebrate({
   params: Joi.object().keys({
-    userId: Joi.string().length(24),
+    cardId: Joi.string().length(24),
   }),
 }), deleteCardById);
 
 router.put('/:cardId/likes', celebrate({
   params: Joi.object().keys({
-    userId: Joi.string().length(24),
+    cardId: Joi.string().length(24),
   }),
 }), likeCard);
 
 router.delete('/:cardId/likes', celebrate({
   params: Joi.object().keys({
-    userId: Joi.string().length(24),
+    cardId: Joi.string().length(24),
   }),
 }), dislikeCard);
 
